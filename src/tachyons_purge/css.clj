538a68ed71ab81604f7d20edfcb140a426eed7bf
Clemens Beer <clemens.beer@gmail.com>
2026-01-04 16:14:21 +0100

    Add @media block support to CSS parser
    The parser now correctly extracts CSS rules from within @media blocks,
    preserving the media query string in the :media field.

--- src/tachyons_purge/css.clj
+++ src/tachyons_purge/css.clj
@@ -1,13 +1,40 @@
 (ns tachyons-purge.css
   (:require [clojure.string :as str]))
 
-(defn parse-css-blocks
-  "Parse CSS into blocks with class names and rules"
-  [css-content]
+(defn- parse-rules-with-media
+  "Parse CSS rules from content and associate with media query (or nil)"
+  [content media]
   (let [pattern #"\.([a-zA-Z0-9_-]+)\s*\{[^}]+\}"
-        matches (re-seq pattern css-content)]
+        matches (re-seq pattern content)]
     (mapv (fn [[rule class-name]]
             {:class class-name
              :rule rule
-             :media nil})
+             :media media})
           matches)))
+
+(defn- extract-media-blocks
+  "Extract @media blocks from CSS, returning pairs of [media-query content]"
+  [css-content]
+  (let [pattern #"@media\s*([^{]+)\s*\{((?:[^{}]|\{[^{}]*\})*)\}"
+        matches (re-seq pattern css-content)]
+    (mapv (fn [[_ query content]]
+            [(str "@media " (str/trim query)) content])
+          matches)))
+
+(defn- remove-media-blocks
+  "Remove @media blocks from CSS content"
+  [css-content]
+  (str/replace css-content #"@media\s*[^{]+\s*\{(?:[^{}]|\{[^{}]*\})*\}" ""))
+
+(defn parse-css-blocks
+  "Parse CSS into blocks with class names and rules"
+  [css-content]
+  (let [;; Parse rules outside @media blocks
+        non-media-content (remove-media-blocks css-content)
+        non-media-rules (parse-rules-with-media non-media-content nil)
+        ;; Parse rules inside @media blocks
+        media-blocks (extract-media-blocks css-content)
+        media-rules (mapcat (fn [[media content]]
+                              (parse-rules-with-media content media))
+                            media-blocks)]
+    (vec (concat non-media-rules media-rules))))
--- test/tachyons_purge/css_test.clj
+++ test/tachyons_purge/css_test.clj
@@ -1,5 +1,6 @@
 (ns tachyons-purge.css-test
   (:require [clojure.test :refer [deftest is testing]]
+            [clojure.string :as str]
             [tachyons-purge.css :as css]))
 
 (deftest parse-css-blocks-test
@@ -9,3 +10,11 @@
       (is (= (count blocks) 2))
       (is (= (:class (first blocks)) "pa0"))
       (is (= (:class (second blocks)) "pa3")))))
+
+(deftest parse-media-blocks-test
+  (testing "parses @media blocks"
+    (let [css "@media screen and (min-width: 30em) {\n  .pa3-ns { padding: 1rem; }\n}"
+          blocks (css/parse-css-blocks css)]
+      (is (= (count blocks) 1))
+      (is (= (:class (first blocks)) "pa3-ns"))
+      (is (str/includes? (:media (first blocks)) "min-width: 30em")))))
